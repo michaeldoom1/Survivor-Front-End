45adebf00656b4bbb625b852b189723c5270be36
michaeldoom <michael.l.doom@gmail.com>
2026-07-11 15:44:19 -0400

    Scaffold React project with Vite and add welcome page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="welcome">
+      <h1>Welcome to Survivor</h1>
+      <p>Your React app is up and running. Let's build something great.</p>
+    </div>
+  )
+}
+
+export default App
